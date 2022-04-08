7eb88daf03688d9ddc342952f8d9434ee2dc2b67
TimzOwen <ktimothy@kabarak.ac.ke>
2022-04-08 14:39:22 +0300

    setup complete

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint = "http://httpbin.org/status/200"
-endpoint = "http://httpbin.org/anything"
+endpoint = "http://httpbin.org/status/200/"
+# endpoint = "http://httpbin.org/anything"
 
-get_response = requests.get(endpoint)
-print(get_response) # in text format
+get_response = requests.get(endpoint, data={"query":"Hello query"})
+print(get_response.json()) # in text format
